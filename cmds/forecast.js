eccbf87616314afc2c62fc1583a62a3bfbf1b56a
Shivammani19 <shivamtripathi214@gmail.com>
2020-02-08 22:07:02 +0530

    ora waiting

--- cmds/forecast.js
+++ cmds/forecast.js
@@ -1,11 +1,15 @@
 const GetRawdata=require('../utilities/getRawWeather');
 const GetLocation=require('../utilities/getLocation')
+const ora=require('ora')
 module.exports=function forecast(location){
+    const spinner = ora();
+    spinner.start();
 
 if(location === undefined){
     GetLocation()
    .then((data)=>{
     const loc=data;
+
     console.log(`Forecast For ${loc} :`)
     GetRawdata(loc)
     .then((data)=>{
@@ -15,6 +19,7 @@ if(location === undefined){
     const currentstate=fulldata.weather_state_name;
     const low=Math.ceil(fulldata.min_temp);
     const high=Math.ceil(fulldata.max_temp);
+    spinner.stop();
     console.log(`${date} - Low: ${low} | High: ${high} | ${currentstate}`)
 }
     })
@@ -26,7 +31,10 @@ if(location === undefined){
 
 }
 
+
+
 else{
+    spinner.start();
     GetRawdata(location)
     .then((data)=>{
 for(let i=0;i<data.length;i++){
@@ -35,6 +43,7 @@ for(let i=0;i<data.length;i++){
     const currentstate=fulldata.weather_state_name;
     const low=Math.ceil(fulldata.min_temp);
     const high=Math.ceil(fulldata.max_temp);
+    spinner.stop();
     console.log(`${date} - Low: ${low} | High: ${high} | ${currentstate}`)
 }
     })
--- cmds/today.js
+++ cmds/today.js
@@ -1,18 +1,25 @@
 const GetRawdata=require('../utilities/getRawWeather');
 const Getlocation=require('../utilities/getLocation')
+const ora=require('ora')
 module.exports=function today(location){
+    const spinner = ora();
+    spinner.start();
 
 if(location === undefined){
     Getlocation()
     .then((data)=>{
+
         const loc=data;
     GetRawdata(data) 
     .then((data)=>{
         //console.log(data)
+
       const fulldata=data[0];
       const currentstate=fulldata.weather_state_name;
       const temp=fulldata.the_temp;
-      const printtemp=Math.ceil(temp)
+      const printtemp=Math.ceil(temp);
+      const spinner = ora();
+  
       console.log(`Current conditions in  ${loc}
               ${printtemp} ${currentstate}`)
     })
@@ -24,14 +31,16 @@ if(location === undefined){
     })
   
 }
-else{
 
+else{
+    
     GetRawdata(location)
     .then((data)=>{
       const fulldata=data[0];
       const currentstate=fulldata.weather_state_name;
       const temp=fulldata.the_temp;
       const printtemp=Math.ceil(temp)
+      spinner.stop();
       console.log(`Current conditions in  ${location}
               ${printtemp} ${currentstate}`)
     })
